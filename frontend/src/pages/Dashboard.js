0ab7d6ef2292d226fa29c4e334a62c893aa8a93b
antoniodavid.bdeo <antoniodavid@bdeo.io>
2024-07-09 19:32:40 +0200

    feat: primera solución sin funcionar

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddCandidateForm from '../components/AddCandidateForm';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <AddCandidateForm />
+    </div>
+  );
+};
+
+export default Dashboard;
