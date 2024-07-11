db691621aaa934631790140f8df4c1877decde5b
Javier Aguila <sjaca10@gmail.com>
2024-07-11 07:22:55 -0600

    (backend): extract array of encrypted fields

--- backend/src/infrastructure/prisma.ts
+++ backend/src/infrastructure/prisma.ts
@@ -2,12 +2,23 @@ import { PrismaClient } from '@prisma/client';
 import { Applicant } from '../domain/models/Applicant';
 import { encrypt } from '../common/crypt';
 
+const encryptedFields: Array<keyof Applicant> = [
+  'firstName',
+  'lastName',
+  'socialSecurityNumber',
+  'dateOfBirth',
+  'email',
+  'phone',
+  'street',
+  'exteriorNumber',
+  'interiorNumber'
+];
+
 const prisma = new PrismaClient().$extends({
   query: {
     applicant: {
       async create({ model, operation, args, query }) {
         // Encrypt PII fields before saving
-        const encryptedFields: Array<keyof Applicant> = ['firstName', 'lastName', 'socialSecurityNumber', 'dateOfBirth', 'email', 'phone'];
         encryptedFields.forEach(field => {
           if (args.data[field]) {
             args.data[field] = encrypt(args.data[field].toString());
